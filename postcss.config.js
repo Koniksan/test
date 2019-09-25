548432ba503c1bcb8711cdeb590e16f6c93aece2
Dmitrij Kisel <Dmitrij.Kisel@elearningforce.com>
2019-09-25 17:52:30 +0300

    Update autoprefixer config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     plugins: [
         require('precss')({ /* ...options */ }),
-        require('autoprefixer')({ /* ...options */ })
+        require('autoprefixer')({ 
+            browsers: 'last 2 versions, > 1%'
+         })
     ]
 };
